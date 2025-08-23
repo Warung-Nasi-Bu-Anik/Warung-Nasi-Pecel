06d9d01bcde544e9df07571fdd124e56861c47fa
Warung-Nasi-Bu-Anik <damn325324@gmail.com>
2025-08-23 15:56:50 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "warung-cache-v1";
+const urlsToCache = [
+  "index1.html",
+  "About1.html",
+  "contact1.html",
+  "favicon.jpg",
+  "icon-192.jpg",
+  "icon-512.jpg",
+  "1.jpg",
+  "2.jpg",
+  "3.jpg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Caching files...");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+                  .map(name => caches.delete(name))
+      );
+    })
+  );
+});
